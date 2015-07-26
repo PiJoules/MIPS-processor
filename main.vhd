a9f2d2db34b84cc20244ccdc7a7242fee4596ea6
pijoules <lchan1994@yahoo.com>
2015-07-25 23:16:14 -0400

    added mux

--- main.vhd
+++ main.vhd
@@ -21,6 +21,10 @@ architecture beh of main is
 
 	signal instr_address: std_logic_vector(31 downto 0); -- Address of the next instruction
 	signal instruction: std_logic_vector(31 downto 0); -- The actual instruction to run
+	signal opcode: std_logic_vector(5 downto 0);
+	signal rs, rt, rd, write_reg: std_logic_vector(4 downto 0);
+	signal reg_dest,jump, branch, mem_read, mem_to_reg, mem_write, alu_src, reg_write: std_logic;
+	signal alu_op: std_logic_vector(1 downto 0);
 
 	 -- Enum for checking if the instructions have loaded
 	type state is (loading, ready);
@@ -52,6 +56,22 @@ architecture beh of main is
 			read_data_1, read_data_2: out std_logic_vector(31 downto 0)
 		);
 	end component;
+	component control
+		port (
+			ck: in std_logic;
+			opcode: in std_logic_vector(5 downto 0);
+			reg_dest,jump, branch, mem_read, mem_to_reg, mem_write, alu_src, reg_write: out std_logic;
+			alu_op: out std_logic_vector(1 downto 0)
+		);
+	end component;
+	component mux
+		generic (n: natural:= 1);
+		port (
+			x,y: in std_logic_vector(n-1 downto 0);
+			s: in std_logic;
+			z: out std_logic_vector(n-1 downto 0)
+		);
+	end component;
 
 	begin
 
@@ -65,8 +85,29 @@ architecture beh of main is
 		end case;
 	end process;
 
+	rs <= instruction(25 downto 21);
+	rt <= instruction(20 downto 16);
+	rd <= instruction(15 downto 11);
+
 	Prog_Count: pc port map (en, instr_address); 
 	IM: instruction_memory port map (en, instr_address, instruction);
-	REG: registers port map (en, reg_write => '0', read_reg_1 => instruction(25 downto 21), read_reg_2 => instruction(20 downto 16), write_reg => instruction(15 downto 11), write_data => dummy_vector, read_data_1 => dummy_vector, read_data_2 => dummy_vector);
+	CONTOL: control port map (en, opcode, reg_dest,jump, branch, mem_read, mem_to_reg, mem_write, alu_src, reg_write, alu_op);
+	-- The mux going into Register's Write Register port; chooses between rt and rd
+	MUX1: mux generic map(5) port map (
+		x => rt, 
+		y => rd, 
+		s => reg_dest,
+		z => write_reg
+	);
+	REG: registers port map (
+		en,
+		reg_write => reg_write,
+		read_reg_1 => rs,
+		read_reg_2 => rt,
+		write_reg => write_reg, 
+		write_data => dummy_vector, 
+		read_data_1 => dummy_vector, 
+		read_data_2 => dummy_vector
+	);
 
 end beh;
---
+++ mux.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity mux is 
+	generic (n: natural:= 1); -- number of bits in the choices
+	port (
+		x,y: in std_logic_vector(n-1 downto 0);
+		s: in std_logic;
+		z: out std_logic_vector(n-1 downto 0)
+	);
+end mux;
+
+architecture beh of mux is
+	begin
+	z <= x when (s='0') else y;
+end beh;
