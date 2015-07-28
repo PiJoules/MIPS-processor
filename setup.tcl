75f5d5917871faf43ef4393dcb1fdb5c28ab4ac4
pijoules <lchan1994@yahoo.com>
2015-07-27 21:56:40 -0400

    script for automatically compiling, running, and setting the clock speed in the modelsim command line

---
+++ setup.tcl
@@ -0,0 +1,13 @@
+# Script for automatically doing the stuff I need to do every time I change something in the source
+
+# Comple all the vhdl files
+vcom *.vhd		
+
+# Start the simulation (use eval since already in terminal, otherwise a new window will open)
+eval vsim work.main
+
+# Add the objects to the wave
+add wave -r /* 			
+
+# Force clock to be 10ns
+force -freeze sim:/main/ck 1 0, 0 {5000 ps} -r 10ns
